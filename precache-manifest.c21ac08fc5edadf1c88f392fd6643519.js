99f37dce88c5a8df11a5de2c3f79602f8ea902f6
m00m <mgaat.hkl@gmail.com>
2020-05-07 10:19:53 +0800

    Updates

---
+++ precache-manifest.c21ac08fc5edadf1c88f392fd6643519.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5977c6963b645f350b790a3ccad76e2",
+    "url": "/add-list-react/index.html"
+  },
+  {
+    "revision": "874d58be4eae39473d63",
+    "url": "/add-list-react/static/js/2.1fb049f1.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/add-list-react/static/js/2.1fb049f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523676ed85735224a0e6",
+    "url": "/add-list-react/static/js/main.981308ed.chunk.js"
+  },
+  {
+    "revision": "19572689dfa7f1bf4bf0",
+    "url": "/add-list-react/static/js/runtime-main.b42db743.js"
+  }
+]);
